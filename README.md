2c375693e6d9c0cd58fd41892267b8a70db22a08
lavreek <lavreeksolacki@yandex.ru>
2022-06-27 14:00:28 +0300

    update

---
+++ README.md
@@ -0,0 +1,7 @@
+# Transfer
+
+√ .odt -> (.doc <-> .docx) -> .pdf √
+√ .docx -> .pdf √
+√ .doc -> .pdf √
+√ .odt -> .pdf √
+√ .txt -> .pdf √
--- index.php
+++ index.php
@@ -3,12 +3,14 @@
 	const pdf_dir = __DIR__."/pdf/";
 	const copy_dir = __DIR__."/copy/";
 
+	const convert_to = ".pdf";
+
 	$scan = scandir(word_dir);
-	$scan = array_diff($scan, [".", ".."]);
+	$scan = array_diff($scan, [".", "..", ".gitignore"]);
 
 	foreach ($scan as $key => $value)
 	{
-		if (is_file(word_dir.$value) && $value != '~$rdreset.docx')
+		if (is_file(word_dir.$value))
 		{
 			$word = new COM("Word.Application") or die ("Could not initialise Object.");
 		
@@ -16,16 +18,18 @@
 			$word->DisplayAlerts = 0;
 			$exp = explode(".", $value);
 
-			copy(word_dir."/$value", copy_dir."/hardreset$key.".$exp[1]);	
+			copy(word_dir.$value, copy_dir."hardreset$key.".$exp[1]);	
 			
-			$word->Documents->Open(copy_dir."/hardreset$key.".$exp[1]);
+			$word->Documents->Open(copy_dir."hardreset$key.".$exp[1]);
 
-			// $word->ActiveDocument->SaveAs(__DIR__.'/output2.doc');
-			$word->ActiveDocument->ExportAsFixedFormat(pdf_dir."/$key.pdf", 17, false, 0, 0, 0, 0, 7, true, true, 2, true, true, false);
+			if (convert_to == ".pdf")
+			{
+				$word->ActiveDocument->ExportAsFixedFormat(pdf_dir.$key.convert_to, 17, false, 0, 0, 0, 0, 7, true, true, 2, true, true, false);
+				rename(pdf_dir.$key.convert_to, pdf_dir.$exp[0].convert_to);
+			}
+			else
+				$word->ActiveDocument->SaveAs(pdf_dir.$exp[0].convert_to);
 			
 			$word->Quit(false);
-
-			rename(pdf_dir."/$key.pdf", pdf_dir."/".$exp[0].".pdf");
-			unlink(copy_dir."/hardreset$key.".$exp[1]);
 		}
 	}
